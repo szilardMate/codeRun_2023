67832f5b077964b7953295acdcbd80fc6761328c
ShadowMurderer <catalin.vale5@gmail.com>
2023-04-21 20:36:31 +0300

    "DA"

---
+++ front-end/src/app/Book.ts
@@ -0,0 +1,6 @@
+export interface BookAuthor {
+  id: number;
+  bookName: string;
+  author: string;
+  publisher: string;
+}
--- front-end/src/app/test.service.ts
+++ front-end/src/app/test.service.ts
@@ -1,10 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BookAuthor } from './Book';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TestService {
+  private apiServerUrl = environment.apiBaseUrl;
 
   constructor(private http:HttpClient) { }
 
@@ -12,5 +16,21 @@ export class TestService {
     this.http.get("/", {responseType:"text"}).subscribe(() =>{
       console.log("Connection works");
     })
+  }
+
+  public getBookAuthors(): Observable<BookAuthor[]> {
+    return this.http.get<BookAuthor[]>(`${this.apiServerUrl}/BookAuthor/all`)
+  }
+
+  public addBookAuthor(BookAuthor: BookAuthor): Observable<BookAuthor> {
+    return this.http.post<BookAuthor>(`${this.apiServerUrl}/BookAuthor/add`, BookAuthor)
+  }
+
+  public updateBookAuthor(BookAuthor: BookAuthor): Observable<BookAuthor> {
+    return this.http.put<BookAuthor>(`${this.apiServerUrl}/BookAuthor/update`, BookAuthor)
+  }
+
+  public deleteBookAuthor(BookAuthorId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiServerUrl}/BookAuthor/delete/${BookAuthorId}`)
   }
 }
